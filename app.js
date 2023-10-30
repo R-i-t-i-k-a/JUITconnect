4c337d101bffd63c51c06a132b1bae981658d5dd
RitikaDadwal <ritikadadwal21@gmail.com>
2023-10-31 03:34:27 +0530

    ejs skeleton done

--- app.js
+++ app.js
@@ -5,19 +5,27 @@ app.get("/",function(req,res){
     res.send("Login page to be made here");
 });
 
+app.get("/register",function(req,res){
+    res.send("Login page to be made here");
+});
+
 app.get("/home",function(req,res){
     res.send("Home Page");
 });
 
-app.get("/home/discover",function(req,res){
+app.get("/about",function(req,res){
     res.send("About the website goes here");
 });
 
-app.get("/home/profilesection",function(req,res){
+app.get("/profilesection",function(req,res){
     res.send("Profile section of the user here");
 });
 
-app.get("/home/ask",function(req,res){
+app.get("/ask",function(req,res){
+    res.send("The user can ask questions here");
+});
+
+app.get("/answer",function(req,res){
     res.send("The user can ask questions here");
 });
 
